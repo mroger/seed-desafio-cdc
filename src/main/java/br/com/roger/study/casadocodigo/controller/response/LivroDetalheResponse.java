a2d38d8d7a956f12a715fe484fdc97ed0b8f6b82
Marcos R Oliveira <mroger.oliveira@gmail.com>
2021-04-03 22:10:05 -0300

    Adicao da data publ. Ajustes no JSON

--- src/main/java/br/com/roger/study/casadocodigo/controller/response/LivroDetalheResponse.java
+++ src/main/java/br/com/roger/study/casadocodigo/controller/response/LivroDetalheResponse.java
@@ -3,42 +3,43 @@ package br.com.roger.study.casadocodigo.controller.response;
 import br.com.roger.study.casadocodigo.model.Livro;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 public class LivroDetalheResponse {
 
-    private Long id;
+    private final String titulo;
 
-    private String titulo;
+    private final String resumo;
 
-    private String resumo;
+    private final String sumario;
 
-    private String sumario;
+    private final BigDecimal preco;
 
-    private BigDecimal preco;
+    private final Integer numeroPaginas;
 
-    private Integer numeroPaginas;
+    private final String isbn;
 
-    private String isbn;
+    private final LocalDate dataPublicacao;
 
-    private AutorResponse autor;
+    private final AutorResponse autor;
 
     public LivroDetalheResponse(final Livro livro) {
-        this.id = livro.getId();
         this.titulo = livro.getTitulo();
         this.resumo = livro.getResumo();
         this.sumario = livro.getSumario();
         this.preco = livro.getPreco();
         this.numeroPaginas = livro.getNumeroPaginas();
         this.isbn = livro.getIsbn();
-        this.autor = AutorResponse.fromAttributes(livro.getNomeAutor(), livro.getDescricaoAutor());
+        this.dataPublicacao = livro.getDataPublicacao();
+        this.autor = new AutorResponse(livro.getNomeAutor(), livro.getDescricaoAutor());
     }
 
     public static LivroDetalheResponse fromModel(final Livro livro) {
         return new LivroDetalheResponse(livro);
     }
 
-    public Long getId() {
-        return id;
+    public LocalDate getDataPublicacao() {
+        return dataPublicacao;
     }
 
     public String getTitulo() {
@@ -67,5 +68,24 @@ public class LivroDetalheResponse {
 
     public AutorResponse getAutor() {
         return autor;
+    }
+
+    static class AutorResponse {
+
+        private final String nome;
+        private final String descricao;
+
+        public AutorResponse(String nome, String descricao) {
+            this.nome = nome;
+            this.descricao = descricao;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public String getDescricao() {
+            return descricao;
+        }
     }
 }
